7f0dfafd2d95ac2c20eeff1190242d8a789a9dd9
Lei Yuan <Yuand2010@hotmail.com>
2023-03-16 21:57:14 +0800

    get data and set excel

--- worm.js
+++ worm.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const puppeteer = require('puppeteer');
-// const request = require('request');
+const request = require('request');
 const fs = require('fs');
 const excel4node = require('excel4node');
 
@@ -36,65 +36,6 @@ app.use('/', function (req, res, next) {
 	}
 });
 
-const asycUrl = async (firstUrl) => {
-	const browser = await puppeteer.launch();
-	const page = await browser.newPage();
-	await page.goto(firstUrl, {
-		waitUntil: 'domcontentloaded',
-	});
-
-	page.on('console', (msg) => console.log('console.log:', msg.text()));
-	// page.on('request', function (req) {
-	//     console.log(req.url());
-	// });
-	page.on('response', function (res) {
-		let reqUrl = res.request().url();
-		if (reqUrl.includes('readSharedViewData')) {
-			browser.close();
-			return res.json();
-		}
-	});
-
-	// const str = await page.content();
-	// if (str.includes('<iframe')) {
-	//     let temp = str.substring(str.indexOf('<iframe'), str.indexOf('</iframe'));
-	//     temp = temp.substring(temp.indexOf('src=') + 5);
-	//     secUrl = temp.substring(0, temp.indexOf('"'));
-	// }
-
-	//注入脚本
-	// const dimensions = await page.evaluate(async () => {
-	//     let span = document.createElement('span');
-	//     span.id = 'custom_res';
-	//     document.body.append(span);
-	//     let script = document.createElement('script');
-	//     script.innerText = `
-	//         const url = 'https://airtable.com/v0.3/view/viwpPe6EkDqbE7Q2o/readSharedViewData?stringifiedObjectParams=%7B%22shouldUseNestedResponseFormat%22%3Atrue%7D&requestId=reqhTRCFZLUfohaAa&accessPolicy=%7B%22allowedActions%22%3A%5B%7B%22modelClassName%22%3A%22view%22%2C%22modelIdSelector%22%3A%22viwpPe6EkDqbE7Q2o%22%2C%22action%22%3A%22readSharedViewData%22%7D%2C%7B%22modelClassName%22%3A%22view%22%2C%22modelIdSelector%22%3A%22viwpPe6EkDqbE7Q2o%22%2C%22action%22%3A%22getMetadataForPrinting%22%7D%2C%7B%22modelClassName%22%3A%22view%22%2C%22modelIdSelector%22%3A%22viwpPe6EkDqbE7Q2o%22%2C%22action%22%3A%22readSignedAttachmentUrls%22%7D%2C%7B%22modelClassName%22%3A%22row%22%2C%22modelIdSelector%22%3A%22rows%20*%5BdisplayedInView%3DviwpPe6EkDqbE7Q2o%5D%22%2C%22action%22%3A%22createBoxDocumentSession%22%7D%2C%7B%22modelClassName%22%3A%22row%22%2C%22modelIdSelector%22%3A%22rows%20*%5BdisplayedInView%3DviwpPe6EkDqbE7Q2o%5D%22%2C%22action%22%3A%22createDocumentPreviewSession%22%7D%5D%2C%22shareId%22%3A%22shrI8dno1rMGKZM8y%22%2C%22applicationId%22%3A%22appPGrJqA2zH65k5I%22%2C%22generationNumber%22%3A0%2C%22expires%22%3A%222023-04-13T00%3A00%3A00.000Z%22%2C%22signature%22%3A%22c4e5f15eff422c66eddae5e7b7544f29c4879a65f11fd26cb0f9f1e1bf9f6bdb%22%7D';
-	//         const options = {
-	//             cache: 'no-cache',
-	//             credentials: 'same-origin',
-	//             headers: {
-	//                 'accept': '*/*'
-	//                 'accept-encoding': 'gzip, deflate, br'
-	//                 'accept-language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7'
-	//             },
-	//             method: 'get',
-	//             mode: 'cors',
-	//             redirect: 'follow',
-	//             referrer: 'no-referrer',
-	//         };
-	//         fetch(url, options).then(res => res.json()).then(data => {console.log(data);document.getElementById('custom_res').innerText=JSON.stringify(data);});
-	//     `;
-	//     document.body.append(script);
-	//     setTimeout(() => {
-	//         return document.getElementById('custom_res').innerText;
-	//     }, 20000);
-
-	// });
-	// fs.writeFileSync('./doc/temp2.html', dimensions);
-	// console.log(dimensions);
-};
-
 let timer = null;
 const clearTimer = function () {
 	if (timer) {
@@ -103,6 +44,18 @@ const clearTimer = function () {
 	timer = null;
 };
 app.post('/api/sendurl', async function (req, res) {
+	// let imgPath = path.join(__dirname, 'images/2img0.jpeg');
+	// request('https://dl.airtable.com/.attachments/15497b643c8079bfaac55f0a0e3b4998/88bd26c5/1656687038505e1683158400vbetatlvgc9AK_kEHuzbXb9wCuW-NFHsgzpE_ta0ysieJkctw', {
+	//     accept: 'image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8',
+	// }).pipe(fs.createWriteStream(imgPath));
+	// request('https://dl.airtable.com/.attachments/15497b643c8079bfaac55f0a0e3b4998/88bd26c5/1656687038505e1683158400vbetatlvgc9AK_kEHuzbXb9wCuW-NFHsgzpE_ta0ysieJkctw', {
+	//     accept: 'image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8',
+	//     encoding: 'binary'
+	// }, function (error, response, body) {
+	//     if (!error) {
+	//         fs.writeFileSync(imgPath, body, 'binary');
+	//     }
+	// })
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
 	await page.goto(req.body.url, {
@@ -125,49 +78,163 @@ app.post('/api/sendurl', async function (req, res) {
 			response.json().then((data) => {
 				clearTimer();
 				browser.close();
-				res.end(JSON.stringify(data)).end();
+				if (data.msg === 'SUCCESS') {
+					const path = generateXlsx(data.data);
+					res.send({ url: path }).end();
+				} else {
+					res.send({ url: '' }).end();
+				}
 			});
 		}
 	});
 });
 
-const asycFile = async (secUrl) => {
-	const browser = await puppeteer.launch();
-	const page = await browser.newPage();
-	await page.goto(secUrl, {
-		waitUntil: 'load',
+const generateXlsx = (data) => {
+	if (!data || !data.table || !data.table.columns || !data.table.rows) {
+		return '';
+	}
+	const wb = new excel4node.Workbook();
+	const ws = wb.addWorksheet('Sheet', {
+		sheetFormat: {
+			defaultRowHeight: 56,
+		},
+	});
+	const cellStyle = wb.createStyle({
+		alignment: {
+			horizontal: 'center',
+			vertical: 'center',
+			wrapText: true,
+		},
+	});
+	ws.row(1).setHeight(20).freeze();
+	const colWidths = [
+		120, 60, 85, 115, 365, 200, 312, 190, 136, 107, 135, 297, 526, 454, 179,
+	];
+	for (let c = 0; c < colWidths.length; c++) {
+		const wcol = colWidths[c] / 8;
+		ws.column(c + 1).setWidth(wcol);
+	}
+	const oriColumns = [].concat(data.table.columns);
+	const columns = [],
+		mapColumns = [],
+		colMap = {};
+	oriColumns.forEach((col, i) => {
+		columns.push(col.id);
+		if (
+			col.typeOptions &&
+			col.typeOptions.choices &&
+			Object.keys(col.typeOptions.choices).length > 0
+		) {
+			mapColumns.push(col.id);
+			for (const key in col.typeOptions.choices) {
+				colMap[key] = col.typeOptions.choices[key].name;
+			}
+		}
+		ws.cell(1, i + 1)
+			.string(col.name)
+			.style(cellStyle);
+	});
+	let imgColIndex = 0;
+	data.table.rows.forEach((row, k) => {
+		for (let m = 0; m < columns.length; m++) {
+			const id = columns[m];
+			const cell = row.cellValuesByColumnId[id];
+			let str = '';
+			if (mapColumns.includes(id)) {
+				if (Array.isArray(cell)) {
+					let arr = [];
+					for (let n = 0; n < cell.length; n++) {
+						arr.push(colMap[cell[n]]);
+					}
+					str = arr.join(', ');
+					ws.cell(k + 2, m + 1)
+						.string(str)
+						.style(cellStyle);
+				} else if (oriColumns[m].type === 'lookup') {
+					if (cell && cell.foreignRowIdOrder) {
+						let tempKeys = cell.foreignRowIdOrder;
+						let tempArr = [];
+						for (let aa = 0; aa < tempKeys.length; aa++) {
+							tempArr.push(
+								colMap[
+									cell.valuesByForeignRowId[tempKeys[aa]][0]
+								]
+							);
+						}
+						ws.cell(k + 2, m + 1)
+							.string(tempArr.join(', '))
+							.style(cellStyle);
+					}
+				}
+			} else if (typeof cell === 'string') {
+				str = cell;
+				ws.cell(k + 2, m + 1)
+					.string(str)
+					.style(cellStyle);
+			} else if (
+				oriColumns[m].type === 'multipleAttachment' &&
+				Array.isArray(cell)
+			) {
+				// let imgPath = './images/img' + k + '.jpeg';
+				// // const imgRes = request(cell[0].url, {
+				// //     accept: 'image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8',
+				// //     encoding: 'binary'
+				// // });
+				// // fs.writeFileSync(imgPath, imgRes.body, 'binary');
+				// request(cell[0].url, {
+				//     accept: 'image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8',
+				// }).pipe(fs.createWriteStream(imgPath));
+				imgColIndex = m;
+				ws.cell(k + 2, m + 1)
+					.link(cell[0].url, 'Logo')
+					.style(cellStyle);
+			} else if (typeof cell === 'number') {
+				if (oriColumns[m].typeOptions.format.includes('percent')) {
+					str = parseInt(cell * 100, 10) + '%';
+					ws.cell(k + 2, m + 1)
+						.string(str)
+						.style(cellStyle);
+				} else {
+					ws.cell(k + 2, m + 1)
+						.number(cell)
+						.style(cellStyle);
+				}
+			} else if (oriColumns[m].type === 'button') {
+				ws.cell(k + 2, m + 1)
+					.link(cell.url, cell.label)
+					.style(cellStyle);
+			} else {
+				ws.cell(k + 2, m + 1)
+					.string(id)
+					.style(cellStyle);
+			}
+		}
 	});
 
-	page.on('console', (msg) => console.log('console.log:', msg.text()));
-	page.on('request', function (req) {
-		console.log('request url = ' + req.url());
-	});
-	// page.on('response', function (res) {
-	//     console.log('response url = ' + res.request().url());
-	// });
-	//注入脚本
-	// const dimensions = await page.evaluate(async () => {
-	//     let headers = [];
-	//     const headList = document.getElementsByClassName('header');
-	//     for (let i = 0; i < headList.length; i++) {
-	//         headers.push(headList[i].innerText);
+	// console.log('add images!');
+	// data.table.rows.forEach((row, k) => {
+	//     let imgPath = './images/img' + k + '.jpeg';
+	//     const imgFile = fs.readFileSync(imgPath);
+	//     if (imgFile) {
+	//         ws.addImage({
+	//             image: fs.readFileSync(imgPath),
+	//             type: 'picture',
+	//             position: {
+	//                 type: 'oneCellAnchor',
+	//                 from: {
+	//                     col: imgColIndex + 1,
+	//                     colOff: 0,
+	//                     row: k + 2,
+	//                     rowOff: 0
+	//                 }
+	//             }
+	//         });
 	//     }
-	//     return [headers];
 	// });
-
-	// fs.writeFileSync('./doc/temp2.html', dimensions);
-	setTimeout(async () => {
-		await browser.close();
-		// return dimensions;
-		return [];
-	}, 30000);
+	const downPath = path.join(__dirname, 'doc/output.xlsx');
+	wb.write(downPath);
+	return '/doc/output.xlsx';
 };
-
-app.post('/api/getFile', function (req, res) {
-	asycFile(req.body.url).then((data) => {
-		res.send({ data }).end();
-	});
-});
 
 app.listen(httpPort, function () {
 	console.log(`Running on port ${httpPort}!`);
